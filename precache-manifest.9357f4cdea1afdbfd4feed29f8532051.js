81c3be3d223a212fc38da4ab995e7b8b56f97cf3
Andrey Petrenko <123petrotreiler321@gmail.com>
2020-10-09 23:27:53 +0300

    Updates

---
+++ precache-manifest.9357f4cdea1afdbfd4feed29f8532051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a2720e4bc2bb7b5e02ea3c2bcdd658",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "6c44652d99be75dd737d",
+    "url": "/redux-todo/static/css/main.6c1bba0c.chunk.css"
+  },
+  {
+    "revision": "c63156cc8be2573f3e5e",
+    "url": "/redux-todo/static/js/2.0723d367.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-todo/static/js/2.0723d367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c44652d99be75dd737d",
+    "url": "/redux-todo/static/js/main.9627f609.chunk.js"
+  },
+  {
+    "revision": "04d5bb36b6a368a6f876",
+    "url": "/redux-todo/static/js/runtime-main.b24b8579.js"
+  }
+]);
